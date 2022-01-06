ed998643fe294899f8cb3e2340fb56325851ef2a
Taiga Miura <miura.t84@gmail.com>
2022-01-06 20:01:01 +0900

    0106

--- 226D.cpp
+++ 226D.cpp
@@ -6,6 +6,8 @@
 #include <queue>
 #include <cmath>
 #include <map>
+#include <cassert>
+#include <numeric>
 using namespace std;
 
 typedef long long ll;
@@ -13,7 +15,6 @@ int inf = numeric_limits<int>::max();
 ll INF = numeric_limits<ll>::max();
 
 #define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
-
 
 int main(){
     int N;
@@ -29,7 +30,6 @@ int main(){
     map<pair<int,int>,bool> list;
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
-            int gcd = 1;
             if(i != j){
                 int x = point[i].first - point[j].first;
                 int y = point[i].second - point[j].second;
@@ -37,31 +37,20 @@ int main(){
                 int z;
                 pair<int,int> p2;
                 if(x == 0){
-                    z = y;
                     p2.first = 0;
                     p2.second = y / abs(y);
                     list[p2] = true;
                 }else if(y == 0){
-                    z = x;
                     p2.first = x / abs(x);
                     p2.second = 0;
                     list[p2] = true;
                 }else{
-                    z = min(abs(x),abs(y));
-                    for(int k = 1; k*k < z; k++){
-                        if(x % k == 0 && y % k == 0){
-                            gcd = k;
-                        }
-                    }
-                    if(x % z == 0 && y % z == 0){
-                        gcd = z;
-                    }
-                    p2.first = x / gcd;
-                    p2.second = y / gcd;
+                    z = gcd(abs(x),abs(y));
+                    p2.first = x / z;
+                    p2.second = y / z;
                     list[p2] = true;
                 }
-                // cout << "ans: " << p2.first << " " << p2.second << endl;
-                
+          
 
             }
         }
