881c5b065f3cfd46df7758cce7dab750a9d8f60c
Taiga Miura <miura.t84@gmail.com>
2022-01-20 00:15:18 +0900

    0120

---
+++ 223D.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+#include <cmath>
+#include <map>
+
+using namespace std;
+
+typedef long long ll;
+int inf = numeric_limits<int>::max();
+ll INF = numeric_limits<ll>::max();
+
+#define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
+
+// 0120 トポロジカルソート　わからなかった。
+
+int main(){
+    int N,M;
+    cin >> N >> M;
+
+    rep(i,0,M){
+
+    }
+}
