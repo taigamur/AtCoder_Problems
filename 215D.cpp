6d441a944a0705d80b5a88bdc1467bb2dbacf231
Taiga Miura <miura.t84@gmail.com>
2022-01-22 00:31:56 +0900

    0122

--- 215D.cpp
+++ 215D.cpp
@@ -5,61 +5,58 @@
 #include <set>
 #include <queue>
 #include <cmath>
+#include <map>
+
 using namespace std;
+
 typedef long long ll;
 int inf = numeric_limits<int>::max();
 ll INF = numeric_limits<ll>::max();
 
 #define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
 
-// 1102 解けていない
+#define SIZE  100005
+
+vector<int> pfact(int x){
+    vector<int> res;
+    for(int i = 2; i*i <= x; i++){
+        while(x%i == 0){
+            x/=i;
+            res.push_back(i);
+        }
+    }
+    if(x!=1){res.push_back(x);}
+    return res;
+}
 
 int main(){
-
-    int N,M;
-    cin >> N >> M;
-    set<int> list;
-    rep(i,0,N){
-        int t;
-        cin >> t;
-        int u = 0;
-        for(int j = 2; j*j <= t; j++){
-            if(t % j == 0){
-                list.insert(j);
-                u++;
+    int n,m;
+    cin >> n >> m;
+    vector<bool> fl(SIZE,true);
+    rep(i,0,n){
+        int a;
+        cin >> a;
+        vector<int> v=pfact(a);
+        rep(i,0,v.size()){
+            if(fl[v[i]]){
+                for(int j=v[i]; j < SIZE; j+=v[i]){
+                    fl[j] = false;
+                }
             }
-        }
-        if(u == 0){
-            list.insert(t);
+
         }
     }
 
-    vector<int> ans;
-    int u = 0;
-    for(int i = 1; i*i <= M; i++){
-
-        int cnt = 0;
-        for(auto t : list){
-            if(i % t != 0){
-                cnt++;
-            }
+    vector<int> res;
+    for(int i=1; i<=m;i++){
+        if(fl[i]){
+            res.push_back(i);
         }
-
-        if(cnt == list.size()){
-            ans.push_back(i);
-        }
-
     }
 
-
-    sort(ans.begin(),ans.end());
-
-    cout << ans.size() << " ";
-    rep(i,0,ans.size()-1){
-        cout << ans[i] << " ";
+    cout << res.size() << endl;
+    for(int i = 0; i < res.size(); i++){
+        cout << res[i] << endl;
     }
-    cout << ans[ans.size()-1] << endl;
-
-
 
 }
