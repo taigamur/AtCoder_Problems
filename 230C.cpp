bed2636542109e51c73b5ccbb3b5fa33d5b45655
Taiga Miura <miura.t84@gmail.com>
2022-06-09 21:07:46 +0900

    230C

--- 230C.cpp
+++ 230C.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -10,25 +11,25 @@
 #include <math.h>
 #include <iomanip>
 #include <limits>
- 
+
 using namespace std;
- 
+
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<long long> vll;
- 
+
 int inf = numeric_limits<int>::max();
 ll INF = numeric_limits<ll>::max();
- 
+
 #define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
 static const int MAX = 100005;
 #define SIZE 10005
- 
+
 struct Node{
     int right, left, parent;
 };
 // struct Node node[SIZE];
- 
+
 class Edge{
     public: 
     int t, w;
@@ -36,31 +37,39 @@ class Edge{
     Edge(int t, int w): t(t), w(w){};
 };
 // vector<Edge> G[MAX];
- 
- 
- 
+
+
+
 int main(){
- 
-    ll T;
-    cin >> T;
- 
-    rep(i,0,T){
-        ll a,s;
-        cin >> a >> s;
-    
-        if(2 * a <= s){
-            ll b = s-2*a;
-            if((b & a) == 0) {
-                cout << "Yes" << endl;
+    ll N,a,b;
+    cin >> N >> a >> b;
+    ll p,q,r,s;
+    cin >> p >> q >> r >> s;
+
+    vector<vector<ll>> list(q - p + 1, vector<ll>( s - r + 1 ,0));
+
+    ll x,y;
+    x = max(p - a, r - b);
+    y = min(q - a, s - b);
+
+    for(ll i = x; i <= y; i++) list[a + i - p][b + i - r] = 1;
+
+    x = max(p - a, b - s);
+    y = min(q - a, b - r);
+    for(ll i = x; i <= y; i++) list[a + i - p][b - i - r] = 1;
+
+    for(ll i = 0; i < (q - p + 1); i++){
+        for(ll j = 0; j < (s - r + 1 ); j++){
+
+            if(list[i][j] == 1){
+                cout << '#';
             }else{
-                cout << "No" << endl;
+                cout << '.';
             }
-        }else{
-            cout << "No" << endl;
         }
+        cout << endl;
     }
- 
- 
- 
- 
+
+
+    return 0;
 }
