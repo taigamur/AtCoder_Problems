aa43929f944da87fcf247fc7802d12d8995e90c5
Taiga Miura <miura.t84@gmail.com>
2022-06-14 20:19:45 +0900

    MAP 区間内の要素の個数

---
+++ 210C.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <math.h>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+
+int inf = numeric_limits<int>::max();
+ll INF = numeric_limits<ll>::max();
+
+#define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
+static const int MAX = 100005;
+#define SIZE 10005
+
+struct Node{
+    int right, left, parent;
+};
+// struct Node node[SIZE];
+
+class Edge{
+    public: 
+    int t, w;
+    Edge();
+    Edge(int t, int w): t(t), w(w){};
+};
+// vector<Edge> G[MAX];
+
+
+
+int main(){
+    int N,K;
+    cin >> N >> K;
+
+    map<ll,ll> m;
+
+    vll list(N);
+
+    int ans = 0;
+
+    rep(i,0,N){
+        ll c;
+        cin >> c;
+        list[i] = c;
+        if(i < K){
+            m[c] += 1;
+        }else{
+            m[c] += 1;
+            if(m[list[i-K]] == 1){
+                m.erase(list[i-K]);
+            }else{
+                m[list[i-K]]--;
+            }
+
+            
+        }
+        if(ans < m.size()) ans = m.size();
+    }
+
+    cout << ans << endl;
+
+
+}
