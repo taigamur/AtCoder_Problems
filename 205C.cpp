9b5635a206bc7355ac0d8729177fecc7fd6b0b4b
Taiga Miura <miura.t84@gmail.com>
2022-06-16 23:42:06 +0900

    指数関数の大きさの場合分け

---
+++ 205C.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <math.h>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+
+int inf = numeric_limits<int>::max();
+ll INF = numeric_limits<ll>::max();
+
+#define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
+static const int MAX = 100005;
+#define SIZE 10005
+
+struct Node{
+    int right, left, parent;
+};
+// struct Node node[SIZE];
+
+class Edge{
+    public: 
+    int t, w;
+    Edge();
+    Edge(int t, int w): t(t), w(w){};
+};
+// vector<Edge> G[MAX];
+
+
+
+int main(){
+
+    ll A,B,C;
+    cin >> A >> B >> C;
+
+    if(C % 2 == 0){
+        if(abs(A) == abs(B)) cout << "=" << endl;
+        else if(abs(A) < abs(B)) cout << "<" << endl;
+        else cout << ">" << endl;
+    }else{
+        if(A == B) cout << "=" << endl;
+        else if(A > B) cout << ">" << endl;
+        else cout << "<" << endl;
+    }
+}
