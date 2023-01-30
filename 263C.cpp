cd216fa9ebd96b286be5ba2a417def1adabded9c
Taiga Miura <miura.t84@gmail.com>
2023-01-30 10:46:30 +0900

    263

---
+++ 263C.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <math.h>
+#include <iomanip>
+#include <limits>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+
+int inf = numeric_limits<int>::max();
+ll INF = numeric_limits<ll>::max();
+
+#define rep(i, s, n) for (int i = (s); i < (int)(n); i++)
+static const int MAX = 100005;
+#define SIZE 10005
+
+struct Node{
+    int right, left, parent;
+};
+// struct Node node[SIZE];
+
+int main(){
+    int N, M;
+    cin >> N >> M;
+
+    vi S(N);
+    rep(i,0,N) S[i] = i+1;
+
+    // while(1){
+
+
+
+    //     rep(i,0,N){
+    //         cout << S[i] << " ";
+    //     }
+    //     cout << endl;
+    
+    // }
+
+    // rep(i,0,10){
+    //     rep(j,,M){
+
+    //     }
+    // }
+
+    rep(i,0,N){
+        
+    }
+}
